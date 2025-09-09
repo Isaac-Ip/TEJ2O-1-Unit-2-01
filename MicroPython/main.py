d6f5552d060feb1d9ebf82e9a358dc1830ebdeff
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-09 13:19:40 -0400

    save

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,11 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
+Created by: Isaac Ip
+Created on: Sep 2025
 This module is a Micro:bit MicroPython program
+This program shows my name.
 """
 
 from microbit import *
 
 
-display.scroll("Hello, World!")
+display.scroll("Isaac Ip")
